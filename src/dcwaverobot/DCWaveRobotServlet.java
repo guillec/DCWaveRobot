2fe921f36152be8eb19f65213e45ea03a2191854
Guille Carlos <g@guillecarlos.com>
2009-11-17 22:39:15 -0500

    Added better parsing, new groovy jar and weather query for DC

--- src/dcwaverobot/DCWaveRobotServlet.java
+++ src/dcwaverobot/DCWaveRobotServlet.java
@@ -1,5 +1,7 @@
 package dcwaverobot;
 
+import groovy.lang.GroovyShell;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -31,24 +33,33 @@ public class DCWaveRobotServlet extends AbstractRobotServlet {
       if (e.getType() == EventType.BLIP_SUBMITTED) {
     	  Blip blip = e.getBlip();
     	  String text = blip.getDocument().getText();
+    	  
     	  if (text.contains("dcmetro")) {
-    	    
-    	    URL yahoo;
-			try {
-				yahoo = new URL("http://www.wmata.com/rider_tools/metro_service_status/feeds/rail.xml");
-				URLConnection yc = yahoo.openConnection();
-				BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-				String inputLine;
-
-            while ((inputLine = in.readLine()) != null) 
-            	blip.createChild().getDocument().append(inputLine);
-            	in.close();
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-    	  }
+				StringBuilder script = new StringBuilder();
+				script.append("def rss = new XmlSlurper().parse('http://www.wmata.com/rider_tools/metro_service_status/feeds/rail.xml'); ");
+				script.append("return rss.toString()");
+				executeScript(blip, script.toString());
+				
+    	 } else if (text.contains("dcweather")) {
+    			StringBuilder script = new StringBuilder();
+ 				script.append("def rss = new XmlSlurper().parse('http://weather.yahooapis.com/forecastrss?p=20001'); ");
+ 				script.append("def result = rss.channel.item.title; ");
+ 				script.append("result << ' ' + rss.channel.item.condition.@text; ");
+ 				script.append("return result");
+ 				executeScript(blip, script.toString());
+    	 }
       }
     }
+  }
+  
+  private void executeScript(Blip blip, String script) {  
+		GroovyShell shell = new GroovyShell();  
+		StringBuilder result = new StringBuilder();  
+		try {  
+		  result.append(shell.evaluate(script));  
+		  blip.createChild().getDocument().append(result.toString());
+		} catch (Exception e) {  
+		  result.append(e.toString());  
+		}  
   }
 }
